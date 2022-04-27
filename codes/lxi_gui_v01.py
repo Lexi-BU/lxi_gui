3fee23935cf54696b4dee738893cbf7f511a3c4b
qudsiramiz <qudsiramiz@gmail.com>
2022-04-27 11:39:58 -0400

    added class

--- codes/lxi_gui_v01.py
+++ codes/lxi_gui_v01.py
@@ -114,6 +114,91 @@ def read_csv_hk(file_val=None, t_start=None, t_end=None):
     return df_slice_hk
 
 
+class plot_diff_data():
+
+    def ts_plots(file_name_sci=None, file_name_hk=None, t_start=None, t_end=None):
+        """
+        """
+        if file_name_sci is None:
+            file_name_sci = file_name_sci
+
+        if file_name_hk is None:
+            file_name_hk = file_name_hk
+
+        # Try to convert the start_time and end_time to float or int
+        try:
+            t_start = float(start_time.get())
+        except Exception as e:
+            print(f"Error: {e}")
+            pass
+        try:
+            t_end = float(end_time.get())
+        except Exception as e:
+            print(f"Error: {e}")
+            pass
+        if not isinstance(t_start, (int, float)):
+            t_start = None
+
+        if not isinstance(t_end, (int, float)):
+            t_end = None
+
+        # Read entries from the text boxes
+        try:
+            x_min = float(x_min_entry.get())
+        except Exception:
+            x_min = None
+        try:
+            x_max = float(x_max_entry.get())
+        except Exception:
+            x_max = None
+        try:
+            y_min = float(y_min_entry.get())
+        except Exception:
+            y_min = None
+        try:
+            y_max = float(y_max_entry.get())
+        except Exception:
+            y_max = None
+        try:
+            bins = int(hist_bins_entry.get())
+        except Exception:
+            bins = None
+        try:
+            cmin = int(c_min_entry.get())
+        except Exception:
+            cmin = None
+        try:
+            density = bool(int(density_entry.get()))
+        except Exception:
+            density = None
+
+        if norm_entry.get() == "linear":
+            norm = "linear"
+        elif norm_entry.get() == "log":
+            norm = "log"
+        else:
+            norm = None
+
+        print(f"Density is {density}")
+        df_slice_hk = read_csv_hk(file_val=file_name_hk, t_start=t_start, t_end=t_end)
+        df_slice_sci = read_csv_sci(file_val=file_name_sci, t_start=t_start, t_end=t_end)
+        df_slice_sci = df_slice_sci[df_slice_sci['IsCommanded']==False]
+
+        # Display the time series plot
+        fig1_ts = plot_routines.plot_indiv_time_series(df=df_slice_hk,
+                                                       key=plot_opt_entry_1.get(), ms=2,
+                                                       alpha=1)
+
+        load1_ts = Image.open(f"figures/{plot_opt_entry_1.get()}_time_series_plot.png")
+        # Resize the image to fit the canvas (in pixels)
+        load1_ts = load1_ts.resize((int(fig1_ts.get_figwidth() * 100),
+                                    int(fig1_ts.get_figheight() * 60)))
+        render1_ts = ImageTk.PhotoImage(load1_ts)
+        img1_ts = tk.Label(image=render1_ts)
+        img1_ts.image = render1_ts
+        img1_ts.grid(row=2, column=0, rowspan=3, columnspan=2, sticky="w")
+
+
 def plot_data(file_name_sci=None, file_name_hk=None, t_start=None, t_end=None):
 
     # TODO: Convert this to a class and decouple it into multiple plot routines for more flexibility
@@ -263,6 +348,13 @@ root.columnconfigure(2, {'minsize': 3}, weight=3)
 root.columnconfigure(3, {'minsize': 3}, weight=3)
 root.columnconfigure(4, {'minsize': 3}, weight=1)
 root.columnconfigure(5, {'minsize': 3}, weight=1)
+#root.columnconfigure(6, {'minsize': 3}, weight=6)
+#root.columnconfigure(7, {'minsize': 3}, weight=2)
+#root.columnconfigure(8, {'minsize': 3}, weight=2)
+#root.columnconfigure(9, {'minsize': 3}, weight=2)
+#root.columnconfigure(10, {'minsize': 3}, weight=1)
+
+#root.rowconfigure(9, {'minsize': 3}, weight=1)
 
 root.title("LEXI GUI")
 # Add the lxi logo
@@ -297,7 +389,10 @@ b_file_load_button = tk.Button(root, text="Load binary File", command=open_file_
 b_file_load_button.grid(row=0, column=2, columnspan=1, pady=0, sticky="w")
 
 # List out all the columns in the housekeeping file as options for time series plots
-ts_options = list(df_slice_hk.columns)
+try :
+    ts_options = list(df_slice_hk.columns)
+except :
+    ts_options = ["PinPullerTemp"] * 4
 
 plot_opt_label_1 = tk.Label(root, text="Plot options:", font=font_style_box)
 plot_opt_label_1.grid(row=1, column=0, columnspan=1, pady=0, sticky="w")
@@ -402,7 +497,7 @@ end_time_label = tk.Label(root, text="End Time", font=font_style)
 end_time_label.grid(row=18, column=3, columnspan=1)
 
 # Add a button to plot the data
-plot_button = tk.Button(root, text="Plot", width=10, font=font_style_box, command=plot_data)
+plot_button = tk.Button(root, text="Plot", width=10, font=font_style_box, command=plot_diff_data.ts_plots)
 plot_button.grid(row=18, column=0, columnspan=2, rowspan=1)
 # Add a button to the window to get start time
 #start_button = tk.Button(root, text="Start", command=lambda: print("Start"))
