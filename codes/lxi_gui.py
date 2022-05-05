1f3953f16bce083e5c7f853470d78af7920cfad0
Ramiz Qudsi <qudsiramiz@gmail.com>
2022-05-05 00:13:22 -0400

    scrollbar edits (non-functioning)

--- codes/lxi_gui.py
+++ codes/lxi_gui.py
@@ -140,37 +140,9 @@ root.title("LEXI GUI")
 root.geometry("600x600")
 root.resizable(True, True)
 
-# Add a scrollbar")
-
-
-def populate(frame):
-    '''Put in some fake data'''
-    for row in range(100):
-        tk.Label(frame, text="%s" % row, width=3, borderwidth="1",
-                 relief="solid").grid(row=row, column=0)
-        t = "this is the second column for row %s" % row
-        tk.Label(frame, text=t).grid(row=row, column=1)
-
-
-def onFrameConfigure(canvas):
-    '''Reset the scroll region to encompass the inner frame'''
-    canvas.configure(scrollregion=canvas.bbox("all"))
-
-
-canvas = tk.Canvas(root, borderwidth=0, background="#ffffff")
-frame = tk.Frame(canvas, background="#ffffff")
-vsb = tk.Scrollbar(root, orient="vertical", command=canvas.yview)
-canvas.configure(yscrollcommand=vsb.set)
-vsb.grid(row=0, column=6, sticky="ns")
-canvas.grid(row=0, column=0, columnspan=6, rowspan=19, sticky="nsew")
-#vsb.pack(side="right", fill="y")
-#canvas.pack(side="left", fill="both", expand=True)
-#canvas.create_window((4, 4), window=frame, anchor="nw")
-
-frame.bind("<Configure>", lambda event,
-           canvas=canvas: onFrameConfigure(canvas))
-
-#populate(frame)
+# Add a scrollbar
+scrollbar = tk.Scrollbar(root)
+scrollbar.grid(row=0, column=6, rowspan=12, sticky="nsew")
 
 # Create a label widget and justify the text
 #my_label = tk.Label(root, text="LEXI GUI", font=(
