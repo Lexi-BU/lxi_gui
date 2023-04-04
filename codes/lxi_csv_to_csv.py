accb253f53a9968a7771e0b15214843876118c2c
qudsiramiz <qudsiramiz@gmail.com>
2023-04-04 10:58:27 -0400

    minor edit

--- codes/lxi_csv_to_csv.py
+++ codes/lxi_csv_to_csv.py
@@ -8,7 +8,7 @@ importlib.reload(lxrf)
 
 def lxi_csv_to_csv(df=None, csv_file=None, csv_folder=None, csvs_file=None, csvs_folder=None):
     """
-    Convert a CSV file to a CDF file.
+    Convert a CSV file to a CSV file.
 
     Parameters
     ----------
