cf7a089718fbf696520da497d9390e561fe13c8b
qudsiramiz <qudsiramiz@gmail.com>
2025-02-14 12:21:48 -0500

    added sh file

---
+++ codes/run_quescent_data.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Activate the virtual environment
+source /home/cephadrius/.cache/pypoetry/virtualenvs/lexi-gui-50Fm991W-py3.10/bin/activate
+
+# Run the script
+python /home/cephadrius/Desktop/git/Lexi-BU/lxi_gui/codes/save_quiescent_data.py
