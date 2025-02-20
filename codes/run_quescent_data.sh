00503bab58fcb5448fd2634f84ca20466a44a9fe
Ramiz Qudsi <qudsiramiz@gmail.com>
2025-02-20 12:26:16 -0500

    removed time series related files

--- codes/run_quescent_data.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Activate the virtual environment
-source /home/cephadrius/.cache/pypoetry/virtualenvs/lexi-gui-50Fm991W-py3.10/bin/activate
-
-# Run the script
-python /home/cephadrius/Desktop/git/Lexi-BU/lxi_gui/codes/save_quiescent_data.py
